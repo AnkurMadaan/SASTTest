95fb67c9cd522cf09f2260b55b3485e35436c5e6
AnkurMadaan <ankurmadaan2787@live.in>
2021-09-10 16:38:14 -0400

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -29,4 +29,21 @@ data "aws_iam_policy_document" "unrestrictable" {
   }
 }
 
+# fail
+
+data "aws_iam_policy_document" "fail" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      "*",
+    ]
+  }
+}
+
+# unknown
 
